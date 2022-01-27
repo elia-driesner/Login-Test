9d7a41e3b49cbe462ced45b62d92383958491575
elia_driesner <78803906+eliaDr@users.noreply.github.com>
2022-01-27 08:41:03 +0100

    added ordered login

--- login/src/views/Login.vue
+++ login/src/views/Login.vue
@@ -1,6 +1,20 @@
 <template>
-    <div id="loginCont">
-        <input v-model="userMail" placeholder="Enter your email" />
-        <input v-model="userPassword" placeholder="Enter your password" />
+  <div id="loginCont">
+      <input v-model="userMail" placeholder="Enter your email" />
+      <input v-model="userPassword" placeholder="Enter your password" />
+      <h1>
+          Your Name is: {{userMail}} and your password is: {{userPassword}}
+      </h1>
     </div>
 </template>
+<script>
+
+export default ({
+  data () {
+    return {
+      userMail: 'userMail',
+      userPassword: 'userPassword'
+    }
+  }
+})
+</script>
